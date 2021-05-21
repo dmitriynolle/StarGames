ac224f40afb9c739e6c0331f0db80b5714ab6f1f
Dmitriy <dimusy@mail.ru>
2021-05-21 14:25:58 +0300

    Initial commit

---
+++ desktop/src/ru/gb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.gb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.gb.StarGames;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGames(), config);
+	}
+}
