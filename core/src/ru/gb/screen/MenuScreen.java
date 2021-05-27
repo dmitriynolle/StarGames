f1e9b7dbadf56ef4cacfa89222596c8ed180ceb7
dmitriynolle <dimusy@mail.ru>
2021-05-27 12:45:31 +0300

    lesson work 2

---
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -0,0 +1,45 @@
+package ru.gb.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import ru.gb.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+
+    private Texture img;
+    private Vector2 pos;
+    private Vector2 v;
+
+    @Override
+    public void show() {
+        super.show();
+        img = new Texture("badlogic.jpg");
+        pos = new Vector2();
+        v = new Vector2(2, 1);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        pos.add(v);
+        ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
+        batch.begin();
+        batch.draw(img, pos.x, pos.y);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        img.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
+        return super.touchDown(screenX, screenY, pointer, button);
+    }
+}
