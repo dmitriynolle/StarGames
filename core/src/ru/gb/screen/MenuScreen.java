347ec6a23358dd864aeaa4d6b9316c54520f7a6d
dmitriynolle <dimusy@mail.ru>
2021-05-31 12:59:41 +0300

    home work 2 refactoring

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -3,34 +3,38 @@ package ru.gb.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 import ru.gb.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final float V_LEN = 1.5f;
     private Texture img;
     private Vector2 pos;
-    private Vector2 posNew;
+    private Vector2 touch;
     private Vector2 v;
+    private Vector2 tmp;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
         pos = new Vector2();
-        posNew = new Vector2();
+        touch = new Vector2();
         v = new Vector2();
+        tmp = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        if(posNew.cpy().sub(pos).len() < 5){
-            v.x = 0;
-            v.y = 0;
+        tmp.set(touch);
+        if(tmp.sub(pos).len() <= v.len()){
+            pos.set(touch);
+            v.setZero();
+        } else {
+            pos.add(v);
         }
-        if (pos != posNew) pos.add(v);
         batch.begin();
         batch.draw(img, pos.x, pos.y, 100f, 100f);
         batch.end();
@@ -44,8 +48,8 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        posNew.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v = (posNew.cpy().sub(pos)).nor().scl(3f);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v = (touch.cpy().sub(pos)).setLength(V_LEN);
         System.out.println(v);
         return super.touchDown(screenX, screenY, pointer, button);
     }
