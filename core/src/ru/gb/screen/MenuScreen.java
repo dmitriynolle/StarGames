c5a6dc2ea2ab45fe0095f11f43213b091701852c
dmitriynolle <dimusy@mail.ru>
2021-05-31 15:01:26 +0300

    home work 3

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -3,54 +3,56 @@ package ru.gb.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 import ru.gb.base.BaseScreen;
+import ru.gb.math.Rect;
+import ru.gb.sprite.Background;
+import ru.gb.sprite.BadLogic;
 
 public class MenuScreen extends BaseScreen {
 
-    private static final float V_LEN = 1.5f;
-    private Texture img;
-    private Vector2 pos;
-    private Vector2 touch;
-    private Vector2 v;
-    private Vector2 tmp;
+    private Texture bg;
+    private Background background;
+
+    private Texture bl;
+    private BadLogic badlogic;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        pos = new Vector2();
-        touch = new Vector2();
-        v = new Vector2();
-        tmp = new Vector2();
+        bg = new Texture("fon.jpg");
+        background = new Background(bg);
+        bl = new Texture("badlogic.jpg");
+        badlogic = new BadLogic(bl);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        badlogic.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        tmp.set(touch);
-        if(tmp.sub(pos).len() <= v.len()){
-            pos.set(touch);
-            v.setZero();
-        } else {
-            pos.add(v);
-        }
         batch.begin();
-        batch.draw(img, pos.x, pos.y, 100f, 100f);
+        background.draw(batch);
+        badlogic.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        img.dispose();
+        bg.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v = (touch.cpy().sub(pos)).setLength(V_LEN);
-        System.out.println(v);
-        return super.touchDown(screenX, screenY, pointer, button);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        super.touchDown(touch, pointer, button);
+        badlogic.touchDown(touch, pointer, button);
+        return false;
     }
 }
