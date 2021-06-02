1a1072ee1db6c38ff7bf1220ce70a19fc4364e31
dmitriynolle <dimusy@mail.ru>
2021-06-02 18:08:53 +0300

    home work 4

--- core/src/ru/gb/screen/GameScreen.java
+++ core/src/ru/gb/screen/GameScreen.java
@@ -2,10 +2,13 @@ package ru.gb.screen;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import ru.gb.base.BaseScreen;
 import ru.gb.math.Rect;
 import ru.gb.sprite.Background;
+import ru.gb.sprite.MyShip;
 import ru.gb.sprite.Star;
 
 public class GameScreen extends BaseScreen {
@@ -17,6 +20,7 @@ public class GameScreen extends BaseScreen {
 
     private Background background;
     private Star[] stars;
+    private MyShip myShip;
 
     @Override
     public void show() {
@@ -28,6 +32,7 @@ public class GameScreen extends BaseScreen {
         for (int i = 0; i < stars.length; i++) {
             stars[i] = new Star(atlas);
         }
+        myShip = new MyShip(atlas);
     }
 
     @Override
@@ -43,6 +48,25 @@ public class GameScreen extends BaseScreen {
         for (Star star : stars) {
             star.resize(worldBounds);
         }
+        myShip.resize(worldBounds);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        myShip.touchDown(touch, pointer, button);
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        myShip.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        myShip.keyUp(keycode);
+        return false;
     }
 
     @Override
@@ -56,6 +80,7 @@ public class GameScreen extends BaseScreen {
         for (Star star : stars) {
             star.update(delta);
         }
+        myShip.update(delta);
     }
 
     private void draw() {
@@ -65,6 +90,7 @@ public class GameScreen extends BaseScreen {
         for (Star star : stars) {
             star.draw(batch);
         }
+        myShip.draw(batch);
         batch.end();
     }
 }
