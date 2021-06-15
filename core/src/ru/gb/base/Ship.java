0ef470f17e7bd05f3edca60965e5c305b1259365
dmitriynovikov <dimusy@mail.ru>
2021-06-15 20:51:53 +0300

    lesson 8

--- core/src/ru/gb/base/Ship.java
+++ core/src/ru/gb/base/Ship.java
@@ -14,7 +14,7 @@ public class Ship extends Sprite {
     private static final float DAMAGE_ANIMATE_INTERVAL = 0.1f;
 
     protected Vector2 v0;
-    public Vector2 v;
+    protected Vector2 v;
 
     protected Rect worldBounds;
     protected ExplosionPool explosionPool;
@@ -73,8 +73,16 @@ public class Ship extends Sprite {
         return damage;
     }
 
-    public void setHp(int hp) {
-        this.hp = hp;
+    public int getHp() {
+        return hp;
+    }
+
+    public Vector2 getV() {
+        return v;
+    }
+
+    public void setBulletPos(Vector2 bulletPos) {
+        this.bulletPos.set(bulletPos);
     }
 
     private void shoot() {
